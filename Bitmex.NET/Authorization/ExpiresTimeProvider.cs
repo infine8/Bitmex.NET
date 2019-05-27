452e426bbe10d1d8d1f85b4a7998581b01eb8591
infine@dasuppa.com <infine@dasuppa.com>
2019-05-27 12:32:23 +0300

    refinig

--- Bitmex.NET/Authorization/ExpiresTimeProvider.cs
+++ Bitmex.NET/Authorization/ExpiresTimeProvider.cs
@@ -9,7 +9,7 @@ namespace Bitmex.NET.Authorization
 
     public class ExpiresTimeProvider : IExpiresTimeProvider
     {
-        private const int LifetimeSeconds = 30;
+        private const int LifetimeSeconds = 300;
 
         private static readonly DateTime EpochTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
--- Bitmex.NET/Models/Partials/OrderPOSTRequestParams.cs
+++ Bitmex.NET/Models/Partials/OrderPOSTRequestParams.cs
@@ -4,36 +4,40 @@ namespace Bitmex.NET.Models
 {
     public partial class OrderPOSTRequestParams
     {
-        public static OrderPOSTRequestParams ClosePositionByMarket(string symbol)
-        {
-            return new OrderPOSTRequestParams
-            {
-                Symbol = symbol,
-                ExecInst = "Close"
-            };
-        }
-        public static OrderPOSTRequestParams ClosePositionByLimit(string symbol, decimal price)
+        public static OrderPOSTRequestParams ClosePositionByMarket(string symbol, string comment = null)
         {
             return new OrderPOSTRequestParams
             {
                 Symbol = symbol,
                 ExecInst = "Close",
-                Price = price
+                Text = comment
+            };
+        }
+        public static OrderPOSTRequestParams ClosePositionByLimit(string symbol, decimal price, string comment = null)
+        {
+            return new OrderPOSTRequestParams
+            {
+                Symbol = symbol,
+                ExecInst = "Close",
+                Price = price,
+                Text = comment
             };
         }
 
-        public static OrderPOSTRequestParams CreateSimpleMarket(string symbol, decimal quantity, OrderSide side)
+        public static OrderPOSTRequestParams CreateSimpleMarket(string symbol, decimal quantity, OrderSide side, string comment = null)
         {
             return new OrderPOSTRequestParams
             {
                 Symbol = symbol,
                 Side = Enum.GetName(typeof(OrderSide), side),
                 OrderQty = quantity,
+                DisplayQty = quantity,
                 OrdType = Enum.GetName(typeof(OrderType), OrderType.Market),
+                Text = comment
             };
         }
 
-        public static OrderPOSTRequestParams CreateSimpleLimit(string symbol, decimal quantity, decimal price, OrderSide side)
+        public static OrderPOSTRequestParams CreateSimpleLimit(string symbol, decimal quantity, decimal price, OrderSide side, string comment = null, bool cancelIfExecOnPlace = true)
         {
             return new OrderPOSTRequestParams
             {
@@ -43,14 +47,15 @@ namespace Bitmex.NET.Models
                 OrdType = Enum.GetName(typeof(OrderType), OrderType.Limit),
                 DisplayQty = quantity,
                 Price = price,
-                ExecInst = "ParticipateDoNotInitiate",
+                ExecInst = cancelIfExecOnPlace ? "ParticipateDoNotInitiate" : string.Empty,
+                Text = comment
             };
         }
 
         /// <summary>
         /// Be aware that bitmex takes fee for hiden limit orders
         /// </summary>
-        public static OrderPOSTRequestParams CreateSimpleHidenLimit(string symbol, decimal quantity, decimal price, OrderSide side)
+        public static OrderPOSTRequestParams CreateSimpleHidenLimit(string symbol, decimal quantity, decimal price, OrderSide side, string comment = null, bool cancelIfExecOnPlace = true)
         {
             return new OrderPOSTRequestParams
             {
@@ -60,34 +65,39 @@ namespace Bitmex.NET.Models
                 OrdType = Enum.GetName(typeof(OrderType), OrderType.Limit),
                 DisplayQty = 0,
                 Price = price,
-                ExecInst = "ParticipateDoNotInitiate",
+                ExecInst = cancelIfExecOnPlace ? "ParticipateDoNotInitiate" : string.Empty,
+                Text = comment
             };
         }
 
-        public static OrderPOSTRequestParams CreateMarketStopOrder(string symbol, decimal quantity, decimal stopPrice, OrderSide side)
+        public static OrderPOSTRequestParams CreateMarketStopOrder(string symbol, decimal quantity, decimal stopPrice, OrderSide side, string comment = null, bool reduceOnly = false)
         {
             return new OrderPOSTRequestParams
             {
                 Symbol = symbol,
                 Side = Enum.GetName(typeof(OrderSide), side),
                 OrderQty = quantity,
+                DisplayQty = quantity,
                 OrdType = Enum.GetName(typeof(OrderType), OrderType.Stop),
                 StopPx = stopPrice,
-                ExecInst = "ReduceOnly,LastPrice",
+                ExecInst = (reduceOnly ?  "ReduceOnly," : "") + "LastPrice",
+                Text = comment
             };
         }
 
-        public static OrderPOSTRequestParams CreateLimitStopOrder(string symbol, decimal quantity, decimal stopPrice, decimal price, OrderSide side)
+        public static OrderPOSTRequestParams CreateLimitStopOrder(string symbol, decimal quantity, decimal stopPrice, decimal price, OrderSide side, string comment = null, bool reduceOnly = false)
         {
             return new OrderPOSTRequestParams
             {
                 Symbol = symbol,
                 Side = Enum.GetName(typeof(OrderSide), side),
                 OrderQty = quantity,
+                DisplayQty = quantity,
                 OrdType = Enum.GetName(typeof(OrderType), OrderType.Stop),
                 StopPx = stopPrice,
                 Price = price,
-                ExecInst = "ReduceOnly,LastPrice",
+                ExecInst = (reduceOnly ? "ReduceOnly," : "") + "LastPrice",
+                Text = comment
             };
         }
     }
